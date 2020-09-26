9166990142b5483d812e2099dddd7fde6d3afc57
Dan Brown <brown.danny.j@gmail.com>
2020-09-26 15:35:40 -0500

    cat generator and part of rps game

--- static/js/script.js
+++ static/js/script.js
@@ -12,3 +12,77 @@ function ageInDays() {
 function reset() {
     document.getElementById('ageInDays').remove();
 }
+
+// Challenge 2: Cat Generator
+function generateCat() {
+    fetch('https://api.thecatapi.com/v1/images/search?size=small')
+        .then(response => response.json())
+        .then((data) => {
+            let catsImgUrl = data[0].url
+
+            var image = document.createElement("img");
+            image.setAttribute('src', `${catsImgUrl}`);
+
+            var div = document.getElementById('flex-cat-gen');
+            div.appendChild(image);
+
+        })
+        .catch(err => console.log(err))
+}
+
+// Challenge 3: Rock, Paper, Scissors
+function rpsGame(yourChoice) {
+    console.log(yourChoice);
+    var humanChoice, botChoice;
+    humanChoice = yourChoice.id;
+
+    botChoice = numberToChoice(randToRpsInt());
+    console.log('Computer choice:', botChoice);
+
+    results = decideWinner(humanChoice, botChoice); // [0,1] human lost | bot won
+    console.log(results);
+
+    message = finalMessage(results); // {'message': 'You won!', 'color': 'green'}
+    console.log(message);
+    // rpsFrontEnd(youChoice.id, botChoice, message);
+}
+
+function randToRpsInt() {
+    return Math.floor(Math.random() * 3);
+}
+
+function numberToChoice(number) {
+    return ['rock', 'paper', 'scissors'][number];
+}
+
+function decideWinner(yourChoice, computerChoice) {
+    var rpsDatabase = {
+        'rock': { 'scissors': 1, 'rock': 0.5, 'paper': 0 },
+        'paper': { 'rock': 1, 'paper': 0.5, 'scissors': 0 },
+        'scissors': { 'paper': 1, 'scissors': 0.5, 'rock': 0 }
+    };
+
+    var yourScore = rpsDatabase[yourChoice][computerChoice];
+    var computerScore = rpsDatabase[yourChoice][computerChoice];
+
+    return [yourScore, computerScore];
+}
+
+function finalMessage([yourScore, computerScore]) {
+    if (yourScore === 0) {
+        return { 'message': 'You lost!', 'color': 'red' };
+    } else if (yourScore === 0.5) {
+        return { 'message': 'You tied!', 'color': 'yellow' };
+    } else {
+        return { 'message': 'You won!', 'color': 'green' };
+    }
+}
+
+function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage) {
+    var imagesDatabase = {
+        'rock': document.getElementById('rock').src,
+        'paper': document.getElementById('paper').src,
+        'scissors': document.getElementById('scissors').src
+
+    }
+}
